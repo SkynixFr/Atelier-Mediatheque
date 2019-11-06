07301cc0982b0d80fbca48637da2414db35c5a45
Romain <romain.day.ludovic@gmail.com>
2019-11-06 11:08:54 +0100

    Création de la classe Document

--- Medianet_usagers/src/medianet_usagers/model/Document.php
+++ Medianet_usagers/src/medianet_usagers/model/Document.php
@@ -1,7 +1,15 @@
 <?php 
 
-namespace medianet_usagers\model;
+namespace medianet_usagers\model; // Espace de noms
 
-class Document {
+class Document extends \Illuminate\Database\Eloquent\Model{ //  Définition de la classe
 	
+	// Définition des variables de la table
+	protected $table = 'document';
+	protected $primaryKey = 'ref';
+	protected $timestamps = false;
+
+	public function types(){
+		
+	}
 }

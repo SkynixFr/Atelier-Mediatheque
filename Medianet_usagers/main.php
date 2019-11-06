6960c44c15fb89f9b417fe53be6b5fb693645726
Romain <romain.day.ludovic@gmail.com>
2019-11-06 15:30:19 +0100

    Ajout des routes dans le fichier main.php

--- Medianet_usagers/main.php
+++ Medianet_usagers/main.php
@@ -31,3 +31,17 @@ $document = new Document();
 $motscles = new MotsCles();
 $type = new Type();
 $usager = new Usager();
+
+// Définition de la route par défaut
+$router->setDefaultRoute('/home/');
+
+// Ajout des toutes les routes dans le tableau routes et des alias dans le tableau alias
+$router->addRoutes('home', '/home/', '\medianet_usagers\MedianetUsagersController', 'viewHome');
+$router->addRoutes('search', '/search/', '\medianet_usagers\MedianetUsagersController', 'viewSearch');
+$router->addRoutes('view', '/view/', '\medianet_usagers\MedianetUsagersController', 'viewView');
+$router->addRoutes('user', '/user/', '\medianet_usagers\MedianetUsagersController', 'viewUser');
+$router->addRoutes('login', '/login/', '\medianet_usagers\MedianetUsagersController', 'viewLogin');
+$router->addRoutes('signup', '/signup/', '\medianet_usagers\MedianetUsagersController', 'viewSignup');
+
+// Execution de la méthode de la route
+$router->run();

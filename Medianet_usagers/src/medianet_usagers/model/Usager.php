be4f4a0892b56998049bb9a32213a1b21a0fb804
Zoons57 <hugo.pallara8@etu.univ-lorraine.fr>
2019-11-06 11:31:55 +0100

    Création de la table usager et la méthode emprunt

--- Medianet_usagers/src/medianet_usagers/model/Usager.php
+++ Medianet_usagers/src/medianet_usagers/model/Usager.php
@@ -4,7 +4,23 @@
 namespace medianet_usagers\model;
 
 
-class Usager
+/* Creation de la class usager*/
+
+class Usager extends \Illuminate\Database\Eloquent\Model
 {
-    
+    protected $table = 'Usager';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+/* méthode emprunt*/
+    public function emprunt()
+    {
+        return $this->BelongsToMany('medianet_usagers\model\Document', 'emprunt', 'usager', 'document');
+        /*
+			*	medianet_usagers\model\Document = La classe du model lié
+			*	emprunt = Nom de la table pivot
+			*	usager = Nom de la clé étrangère de la table pivot
+			*	document = Nom clé étrangère de latable qui est présent dans la table document
+			*/
+    }
 }

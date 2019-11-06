902dc51ef512150904dda04272609e8d3c27d490
Zurginho <ludovic.meligner@outlook.fr>
2019-11-06 11:28:59 +0100

    Modification de la classe MotsCles

--- Medianet_usagers/src/medianet_usagers/model/MotsCles.php
+++ Medianet_usagers/src/medianet_usagers/model/MotsCles.php
@@ -2,9 +2,16 @@
 
 namespace medianet_usagers\model;
 
-class MotsCles{
+class MotsCles extends \Illuminate\Database\Eloquent\Model {
 
-	
+    protected $table = 'motscles';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+public function recherche(){
+	return $this -> belongsToMany('medianet_usagers\model\Document','motscles','recherche','reference');
+}
+
 }
 
 

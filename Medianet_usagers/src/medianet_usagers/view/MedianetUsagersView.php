41f6946d94376e29d44a4b2a73f78225769a9084
Romain <romain.day.ludovic@gmail.com>
2019-11-09 08:45:09 +0100

    Modification du css de la vue Home

--- Medianet_usagers/src/medianet_usagers/view/MedianetUsagersView.php
+++ Medianet_usagers/src/medianet_usagers/view/MedianetUsagersView.php
@@ -50,9 +50,11 @@ class MedianetUsagersView extends \mf\view\AbstractView {
 	public function renderFooter(){
 		$footer = '
 			<footer>
-	    		<p> Créée par les steack</p>
-	    		<a href="#">Nous contacter</a>
-	    		<p>Projet CIASIE</p>
+	    		<ul>
+	    			<li>Créée par les steack</li>
+	    			<li><a href="#">Nous contacter</a></li>
+	    			<li>Projet CIASIE</li>
+	    		</ul>
     		</footer>';
 		return $footer;
 	}
@@ -65,7 +67,7 @@ class MedianetUsagersView extends \mf\view\AbstractView {
 				<p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l\'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n\'a pas fait que survivre cinq siècles, mais s\'est aussi adapté à la bureautique informatique, sans que son contenu n\'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker. </p>
 			</article>
 			<article>
-				<p> Besoin d\'aide ? zinniArthur@email.fr pallaraHugo@email.fr melignerLudovic@email.fr dayRomain@email.fr</p>
+				<p> Besoin d\'aide ? zinniArthur@email.fr | pallaraHugo@email.fr | melignerLudovic@email.fr | dayRomain@email.fr</p>
 			</article>
 		</section>
 		<section>
